16a97aae55289a474df1593c1b87c0f176d87aad
Fikret Arıcı <fikretarici35@gmail.com>
2025-06-23 08:44:10 +0300

    seo ayarlamalarıyla uğraşıldı hakkımızda kısmından başlaması engellendi tekrar denendi

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Tarayıcının otomatik scroll yapmasını engelle
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
